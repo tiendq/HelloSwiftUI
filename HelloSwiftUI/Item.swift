4feb657070f5843ba88adb7c8661ec20f500c6a0
Tien Do <tiendq@gmail.com>
2024-05-10 11:15:38 +0700

    Initial Commit

---
+++ HelloSwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HelloSwiftUI
+//
+//  Created by Tien Do on 5/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
